70d0493a74f171e1a03906efd0d52be10b0d6d43
Raul Alberto Niño <raul.nino@est.iudigital.edu.co>
2025-05-11 13:33:40 -0500

    feat: primer comit del proyecto

--- src/edu_pad/dataweb.py
+++ src/edu_pad/dataweb.py
@@ -8,19 +8,18 @@ import datetime
 
 class DataWeb:
     def __init__(self):
-        self.url = "https://es.finance.yahoo.com/quote/DOLA-USD/history/"
+        self.url = "https://es.finance.yahoo.com/quote/GOOG/history"
     
 
     def obtener_datos(self):
         try:
-            # url , cabeceras
             headers = {
                 'User-Agent': 'Mozilla/5.0'
             }
             respuesta = requests.get(self.url,headers=headers)
             if respuesta.status_code != 200:
                 print("La url saco error, no respondio o no existe")
-            #print(respuesta.text)
+            
             soup = BeautifulSoup(respuesta.text,'html.parser')
             tabla = soup.select_one('div[data-testid="history-table"] table')
             nombre_columnas = [th.get_text(strip=True) for th in tabla.thead.find_all('th')]
@@ -43,13 +42,7 @@ class DataWeb:
             print("Datos Obtenidos ")
             print("*******************************************************************")
             print(df.head())
-            #df.to_excel("dataweb_limpio.xlsx")
-            # print(nombre_columnas)
-            # print(filas)
-            # for th in tabla.thead.find_all('th'):
-            #     th_data = th.get_text(strip=True)
-# ['Fecha', 'Abrir', 'Máx.', 'Mín.', 'CerrarPrecio de cierre ajustado para splits.', 'Cierre ajustadoPrecio de cierre ajustado para splits y distribuciones de dividendos o plusvalías.', 'Volumen']
-
+            
             return df
         except Exception as err:
             print("Error en la funcion obtener_datos")
@@ -59,7 +52,6 @@ class DataWeb:
     def convertir_numericos(self,df=pd.DataFrame()):
         df= df.copy()
         if len(df)>0:
-            #for col in (df.columns):
             for col in ('abrir',	'max',	'min',	'cerrar',	'cierre_ajustado',	'volumen'):
                 df[col] = (df[col]
                            .str.replace(r"\.","",regex=True)
@@ -69,11 +61,3 @@ class DataWeb:
 
 
 
-
-# dw = DataWeb()
-# dw.obtener_datos()
-# #print(dw.url)
-
-# nom_columas = ["kjakl","kjkhasd","jklhasd"]
-# columna =     [4654,    6456,     465456]
-# fila =       [[4654,    6456,     465456],[4654,    6456,     465456],[4654,    6456,     465456]]
